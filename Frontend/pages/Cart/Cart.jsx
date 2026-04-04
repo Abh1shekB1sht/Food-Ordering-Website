10d24a0232f46e54e788552d5cfe59821b8463fe
Abhishek <singhbisht84484@gmail.com>
2026-04-04 16:27:31 +0530

    add : Added  Cart component with item listing, totals, and promo code input

--- Frontend/pages/Cart/Cart.jsx
+++ Frontend/pages/Cart/Cart.jsx
@@ -1,8 +1,84 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Cart.css";
+import { StoreContext } from "../../context/StoreContext";
 
 const Cart = () => {
-    return <div>Cart</div>;
+    const { cartItems, food_list, removeFromCart } = useContext(StoreContext);
+    return (
+        <div className="cart">
+            <div className="cart-items">
+                <div className="cart-items-title">
+                    <p>Items</p>
+                    <p>Title</p>
+                    <p>Price</p>
+                    <p>Quantity</p>
+                    <p>Total</p>
+                    <p>Remove</p>
+                </div>
+                <br />
+                <hr />
+                {food_list.map((item) => {
+                    if (cartItems[item._id]) {
+                        return (
+                            <div>
+                                <div className="cart-items-title cart-items-item">
+                                    <img src={item.image} alt={item.name} />
+                                    <p>{item.name}</p>
+                                    <p>${item.price.toFixed(2)}</p>
+                                    <p>{cartItems[item._id]}</p>
+                                    <p>
+                                        $
+                                        {(
+                                            item.price * cartItems[item._id]
+                                        ).toFixed(2)}
+                                    </p>
+                                    <p
+                                        onClick={() => removeFromCart(item._id)}
+                                        className="cross"
+                                    >
+                                        x
+                                    </p>
+                                </div>
+                                <hr />
+                            </div>
+                        );
+                    }
+                })}
+            </div>
+            <div className="cart-bottom">
+                <div className="cart-total">
+                    <h2>Cart Totals</h2>
+                    <div>
+                        <div className="cart-total-details">
+                            <p>SubTotal</p>
+                            <p>{0}</p>
+                        </div>
+                        <hr />
+                        <div className="cart-total-details">
+                            <p>Delivery Fee</p>
+                            <p>{2}</p>
+                        </div>
+                        <hr />
+                        <div className="cart-total-details">
+                            <b>Total</b>
+                            <b>{0}</b>
+                        </div>
+                    </div>
+                    <button>PROCEED TO CHECKOUT</button>
+                </div>
+
+                <div className="cart-promocode">
+                    <div>
+                        <p>If you have a promo code, enter it here</p>
+                        <div className="cart-promocode-input">
+                            <input type="text" placeholder="promo code" />
+                            <button>Submit</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Cart;
