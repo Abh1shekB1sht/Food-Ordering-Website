e3063ce9d568b4ff677a802b8fa8d430f5f76722
Abhishek <singhbisht84484@gmail.com>
2026-04-05 19:14:20 +0530

    fix: update MongoDB connection string for improved access

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
     try {
         await mongoose.connect(
-            "mongodb+srv://singhbisht84484:qTRG3066Lr0klASb@cluster0.ysttxpk.mongodb.net/foodora?retryWrites=true&w=majority",
+            "mongodb://singhbisht84484_db_user:8448463607@ac-gjt0krx-shard-00-00.wqwqt5c.mongodb.net:27017,ac-gjt0krx-shard-00-01.wqwqt5c.mongodb.net:27017,ac-gjt0krx-shard-00-02.wqwqt5c.mongodb.net:27017/?ssl=true&replicaSet=atlas-13tm4m-shard-0&authSource=admin&appName=Cluster0",
         );
         console.log("MongoDB connected successfully");
     } catch (error) {
