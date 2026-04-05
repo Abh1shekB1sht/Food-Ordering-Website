01c550b776b010ddb94dc16dce0e4b9d98ce7888
Abhishek <singhbisht84484@gmail.com>
2026-04-05 23:31:03 +0530

    add: implement user authentication with login and registration functions

---
+++ Backend/controllers/userController.js
@@ -0,0 +1,92 @@
+import userModel from "../models/userModel.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import validator from "validator";
+
+// login user
+const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        // checking if user exists
+        const user = await userModel.findOne({
+            email,
+        });
+        if (!user) {
+            return res.json({ success: false, message: "User doesn't exist" });
+        }
+
+        // comparing password
+        const isMatch = bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            res.json({ success: false, message: "Invalid credentials" });
+        }
+
+        // creating token
+        const token = createToken(user._id);
+        return res.json({
+            success: true,
+            message: "User logged in successfully",
+            token,
+        });
+    } catch (error) {
+        return res.json({ success: false, message: error.message });
+    }
+};
+
+const createToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
+};
+
+// register user
+const registerUser = async (req, res) => {
+    const { name, email, password } = req.body;
+
+    try {
+        // checking if user already exists
+        const exists = await userModel.findOne({
+            email: email,
+        });
+        if (exists) {
+            return res.json({ success: false, message: "User already exists" });
+        }
+
+        // validating email format and strong password
+        if (!validator.isEmail(email)) {
+            return res.json({
+                success: false,
+                message: "Please enter a valid email address",
+            });
+        }
+        if (!validator.isStrongPassword(password)) {
+            return res.json({
+                success: false,
+                message:
+                    "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one symbol",
+            });
+        }
+
+        // hashing password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        // creating new user
+        const newUser = new userModel({
+            name,
+            email,
+            password: hashedPassword,
+        });
+
+        const user = await newUser.save();
+        const token = createToken(user._id);
+
+        return res.json({
+            success: true,
+            message: "User registered successfully",
+            token,
+        });
+    } catch (error) {
+        return res.json({ success: false, message: error.message });
+    }
+};
+
+export { loginUser, registerUser };
