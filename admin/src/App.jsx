60b3e3e3221a699344d0bd58032b0fb1c8969ecc
Abhishek <singhbisht84484@gmail.com>
2026-04-06 14:01:14 +0530

    fix: correct prop passing for Add, List, and Orders components in Routes

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -17,9 +17,9 @@ const App = () => {
             <div className="app-content">
                 <Sidebar />
                 <Routes>
-                    <Route path="/add" url={url} element={<Add />} />
-                    <Route path="/list" url={url} element={<List />} />
-                    <Route path="/orders" url={url} element={<Orders />} />
+                    <Route path="/add" element={<Add url={url} />} />
+                    <Route path="/list" element={<List url={url} />} />
+                    <Route path="/orders" element={<Orders url={url} />} />
                 </Routes>
             </div>
         </div>
