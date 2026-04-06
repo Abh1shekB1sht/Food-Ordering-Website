0607fd934f37fdf621d9b82a01728f7fe930bb12
Abhishek <singhbisht84484@gmail.com>
2026-04-07 00:33:52 +0530

    fix: update import paths for order and user models to include file extensions

--- Backend/controllers/orderController.js
+++ Backend/controllers/orderController.js
@@ -1,5 +1,5 @@
-import orderModel from "../models/orderModel";
-import userModel from "../models/userModel";
+import orderModel from "../models/orderModel.js";
+import userModel from "../models/userModel.js";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
@@ -50,6 +50,7 @@ const placeOrder = async (req, res) => {
         const session = await stripe.checkout.sessions.create({
             line_items: line_items,
             mode: "payment",
+            payment_method_types: ["card"],
             success_url: `${CLIENT_URL}/verify?success=true&orderId=${newOrder._id}`,
             cancel_url: `${CLIENT_URL}/verify?success=false&orderId=${newOrder._id}`,
         });
