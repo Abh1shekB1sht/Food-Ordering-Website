e78a3e429d644537f9a8c0e5dec5b20d4d603767
Abhishek <singhbisht84484@gmail.com>
2026-04-06 14:01:20 +0530

    fix: update image source in FoodItem component to include URL from context

--- Frontend/components/FoodItem/FoodItem.jsx
+++ Frontend/components/FoodItem/FoodItem.jsx
@@ -4,13 +4,14 @@ import { assets } from "../../src/assets/assets";
 import { StoreContext } from "../../context/StoreContext";
 
 const FoodItem = ({ id, name, price, description, image }) => {
-    const { cartItems, addToCart, removeFromCart } = useContext(StoreContext);
+    const { url, cartItems, addToCart, removeFromCart } =
+        useContext(StoreContext);
     return (
         <div className="food-item">
             <div className="food-item-image-container">
                 <img
                     className="food-item-image"
-                    src={image}
+                    src={url + "/images/" + image}
                     alt="image of the food item"
                 />
                 {!cartItems[id] ? (
